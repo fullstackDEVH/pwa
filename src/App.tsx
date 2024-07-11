9f64eff6db2aa92ca1232bca114f58b0b7d51817
fullstackDEVH <huagiahuy20052001@gmail.com>
2024-07-11 23:49:45 +0700

    feat : request notifications in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 // src/App.tsx
 
-import React from "react";
+import React, { useEffect } from "react";
+
+const requestNotificationPermission = async () => {
+  const permission = await Notification.requestPermission();
+  if (permission === "granted") {
+    console.log("Notification permission granted.");
+    alert("granted");
+  } else {
+    console.log("Notification permission denied.");
+    alert("denied");
+  }
+};
 
 const App: React.FC = () => {
+  useEffect(() => {
+    requestNotificationPermission();
+  }, []);
+
   const handlePushNotificationClick = () => {
     if ("serviceWorker" in navigator && Notification.permission === "granted") {
       console.log("send");
-      
+
       navigator.serviceWorker.ready.then((registration) => {
         registration.showNotification("Thông báo từ ứng dụng", {
           body: "Chào mừng bạn đến với ứng dụng của chúng tôi!",
--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,13 @@
 // src/index.tsx
-import React, { useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 // import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-const requestNotificationPermission = async () => {
-  const permission = await Notification.requestPermission();
-  if (permission === "granted") {
-    console.log("Notification permission granted.");
-  } else {
-    console.log("Notification permission denied.");
-  }
-};
-
-const AppWrapper: React.FC = () => {
-  useEffect(() => {
-    requestNotificationPermission();
-  }, []);
-
-  return <App />;
-};
-
 ReactDOM.render(
   <React.StrictMode>
-    <AppWrapper />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
@@ -36,7 +19,7 @@ ReactDOM.render(
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     console.log("load");
-    
+
     navigator.serviceWorker
       .register("/service-worker.js")
       .then((registration) => {
